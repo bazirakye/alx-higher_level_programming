297d406ff24be7e7e582ac34f3f95280395886ac
bazirakye <bazirakyetonny15@gmail.com>
2023-06-06 02:17:07 -0700

    c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+* check_cycle - checks for a cycle in a singly linked list
+* @list: singly list to check
+* Return: 0 if no cycle, 1 if cycle
+*/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *head = list, *tail = list;
+
+	if (list != NULL)
+	{
+		while (head && head->next)
+		{
+			head = head->next->next;
+			tail = tail->next;
+			if (tail == head)
+			{
+				tail = list;
+				while (tail != head)
+				{
+					tail = tail->next;
+					head = head->next;
+				}
+				return (1);
+			}
+		}
+	}
+	return (0);
+}
