388231a93c1da8e58c640550e7d256998d5a9a11
bazirakye tonny <bazirakyetonny15@gmail.com>
2023-06-08 18:54:29 +0000

    sum

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-	from add_0 import add
-	a=1
-	b=2
-	sum = "{} + {} = {}".format(a, b, add(a, b))
-	print(sum)
+if __name__ == '__main__':
+    from add_0 import add
+    a = 1
+    b = 2
+    sum = "{} + {} = {}".format(a, b, sum(a,b))
+    print(sum)
