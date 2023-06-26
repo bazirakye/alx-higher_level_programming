fcc8f826c2e54ef131377a1930dc5fe52bc32353
bazirakye <bazirakyetonny15@gmail.com>
2023-06-26 11:17:16 -0700

    IndexError exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,10 @@ def safe_print_list(my_list=[], x=0):
             print(item, end='')
             count += 1
             if count == x:
-                break  # Stop the loop when the desired number of elements is printed
-        print()  # Print a new line
-        return count  # Return the number of elements printed
+                break
+        print()
+        return count
     except IndexError:
-        return 0  # Return 0 if an exception occurs
+        print()
+        return count
+    return count + 1
