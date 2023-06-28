d5d7a8b9351df237ba516af0740008a4395fae90
bazirakye <bazirakyetonny15@gmail.com>
2023-06-28 11:25:01 -0700

    private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" a class Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """represent square"""
+
+
+    def __init__(self, size):
+        """ init sqaure
+
+            Args:
+                size (int): The size of the new square.
+        """
+        self.__size = size
