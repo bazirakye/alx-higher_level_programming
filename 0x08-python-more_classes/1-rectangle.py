5ebea4702c5c18eb8beec4b6dd7d9a4bd8fb2af6
bazirakye <bazirakyetonny15@gmail.com>
2023-07-03 06:45:57 -0700

    set width and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,7 +5,13 @@
 class Rectangle:
     """represents an empty rectangle"""
 
-    def __init__(self, width = 0, height = 0):
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.width = width
         self.height = height
 
